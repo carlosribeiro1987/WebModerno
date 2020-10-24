a4695a69e95bc845ceafc5747c8f0651ac13ca97
Carlos Ribeiro <rotting-christ_@hotmail.com>
2020-10-24 17:27:25 -0300

    24-10

---
+++ javascript/01-fundamentos/03-tipagemFraca.js
@@ -0,0 +1,11 @@
+let qualquer = 'Legal';
+console.log(qualquer);
+console.log(typeof qualquer); //string
+qualquer = 3.1516;
+console.log(qualquer);
+console.log(typeof qualquer); //number
+
+//Evitar nomes genéricos e siglas
+let valor = '';
+let numero = 1;
+let pqp = false; //Produto Químico Perigoso
